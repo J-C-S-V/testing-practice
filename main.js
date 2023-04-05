6032131c5202163ea21f47978ebc9d48a8bae389
Juan Sanchez <juansan1993@hotmail.com>
2023-04-04 22:45:13 -0500

    Add calculator class and capitalized functions with their tests

--- main.js
+++ main.js
@@ -10,7 +10,30 @@ function reverseString(string) {
   return string.split('').reverse().join('');
 }
 
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+}
+
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+console.log(capitalize('Hello'));
+
 module.exports = {
   stringLength,
   reverseString,
+  Calculator,
+  capitalize,
 };
--- main.test.js
+++ main.test.js
@@ -1,16 +1,53 @@
 const { default: expect } = require('expect');
-const { stringLength, reverseString } = require('./main.js');
+const {
+  stringLength,
+  reverseString,
+  Calculator,
+  capitalize,
+} = require('./main.js');
 
-// Test stringLength
 test('String is ok', () => {
   expect(stringLength('Hello')).toBe(5);
 });
+
 test('String is out of range', () => {
   expect(() => stringLength('123456789hello')).toThrow(Error);
 });
 
-// Test reverseString
-
 test('String is reversed successfully', () => {
   expect(reverseString('Hello')).toBe('olleH');
+});
+
+describe('Test calculator', () => {
+  test('Sum working', () => {
+    const calculator = new Calculator();
+    const calculatorAdd = calculator.add(2, 2);
+
+    expect(calculatorAdd).toBe(4);
+  });
+
+  test('Substraction working', () => {
+    const calculator = new Calculator();
+    const calculatorSub = calculator.subtract(6, 2);
+
+    expect(calculatorSub).toBe(4);
+  });
+
+  test('Multiplication working', () => {
+    const calculator = new Calculator();
+    const calculatorMultiply = calculator.multiply(2, 2);
+
+    expect(calculatorMultiply).toBe(4);
+  });
+
+  test('Division working', () => {
+    const calculator = new Calculator();
+    const calculatorDivision = calculator.divide(8, 2);
+
+    expect(calculatorDivision).toBe(4);
+  });
+});
+
+test('Capitalized string', () => {
+  expect(capitalize('hello')).toBe('Hello');
 });

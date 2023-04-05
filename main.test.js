c5b5ba8747a9205aaf6eb80bd086c09d65093e6b
Juan Sanchez <juansan1993@hotmail.com>
2023-04-04 22:06:45 -0500

    Add reversed string test

--- main.test.js
+++ main.test.js
@@ -1,8 +1,16 @@
-const stringLength = require('./main.js');
+const { default: expect } = require('expect');
+const { stringLength, reverseString } = require('./main.js');
 
+// Test stringLength
 test('String is ok', () => {
   expect(stringLength('Hello')).toBe(5);
 });
 test('String is out of range', () => {
   expect(() => stringLength('123456789hello')).toThrow(Error);
+});
+
+// Test reverseString
+
+test('String is reversed successfully', () => {
+  expect(reverseString('Hello')).toBe('olleH');
 });
